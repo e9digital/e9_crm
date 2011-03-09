965ad92a02d64967563beaaed12625de93a6bfe3
Travis <numbers1311407@gmail.com>
2011-03-09 06:50:44 -0500

    moved mattr accessors up to top level, changed logger

--- lib/e9_crm.rb
+++ lib/e9_crm.rb
@@ -8,6 +8,22 @@ module E9Crm
     autoload :ActiveRecord,     'e9_crm/backend/active_record'
   end
 
+  mattr_accessor :cookie_name
+  @@cookie_name = '_e9_tc'
+
+  mattr_accessor :query_param
+  @@query_param = 'code'
+
+  mattr_accessor :log_level
+  @@log_level = :debug
+
+  mattr_accessor :logging
+  @@logging = true
+
+  def E9Crm.log(message)
+    Rails.logger.send(@@log_level, "e9Crm: #{message}") if @@logging
+  end
+
   class Engine < ::Rails::Engine
     config.e9_crm = E9Crm
   end
--- lib/e9_crm/tracking_controller.rb
+++ lib/e9_crm/tracking_controller.rb
@@ -7,12 +7,6 @@ module E9Crm
 
     included { before_filter :track_page_view }
 
-    mattr_accessor :cookie_name
-    @@cookie_name = '_e9_tc'
-
-    mattr_accessor :query_param
-    @@query_param = 'code'
-
     protected 
 
     def track_page_view
@@ -31,53 +25,49 @@ module E9Crm
 
     private
 
-    def log(msg)
-      Rails.logger.debug("CRM: #{msg}")
-    end
-
     def _load_or_install_tracking_cookie
       @_tracking_cookie ||= begin
-        log "Begin load or install cookie: cookie_name[#{TrackingController.cookie_name}] query_param[#{TrackingController.query_param}]"
+        E9Crm.log "Begin load or install cookie: cookie_name[#{E9Crm.cookie_name}] query_param[#{E9Crm.query_param}]"
 
-        code = params.delete(TrackingController.query_param)
-        tcn  = TrackingController.cookie_name
+        code = params.delete(E9Crm.query_param)
+        tcn  = E9Crm.cookie_name
 
         if hid = cookies[tcn]
-          log("Installed cookie found: hid[#{hid}]")
+          E9Crm.log("Installed cookie found: hid[#{hid}]")
           @_tracking_cookie = TrackingCookie.find_by_hid(hid)
         end
 
-        log(@_tracking_cookie ? "Cookie loaded: (#{tcn} : #{@_tracking_cookie.hid})" : "Cookie not found")
+        E9Crm.log(@_tracking_cookie ? "Cookie loaded: (#{tcn} : #{@_tracking_cookie.hid})" : "Cookie not found")
 
         if @_tracking_cookie.present?
           attrs = {}
 
           if current_user && !@_tracking_cookie.user_id?
-            log("Cookie user (#{@_tracking_cookie.user_id}) not current_user (#{current_user.id}), changing...")
+            E9Crm.log("Cookie user (#{@_tracking_cookie.user_id}) not current_user (#{current_user.id}), changing...")
             attrs[:user] = current_user
           end
 
           if code.present? && code != @_tracking_cookie.code
-            log "Code present and cookie code(#{@_tracking_cookie.code}) does not match (#{code}), changing..."
+            E9Crm.log "Code present and cookie code(#{@_tracking_cookie.code}) does not match (#{code}), changing..."
             attrs[:code] = code
-            log "Cookie marked as new"
+            E9Crm.log "Cookie marked as new"
             @_tracking_cookie.new_visit = true
           end
 
-          log(attrs.blank? ? "Cookie unchanged, no update" : "Cookie changed, new attrs: #{attrs.inspect}")
+          E9Crm.log(attrs.blank? ? "Cookie unchanged, no update" : "Cookie changed, new attrs: #{attrs.inspect}")
           @_tracking_cookie.update_attributes(attrs) unless attrs.blank?
         end
 
         @_tracking_cookie ||= begin
           TrackingCookie.create(:code => code, :user => current_user).tap do |cookie|
-            log "Installing new cookie (#{tcn} : #{cookie.hid})"
+            E9Crm.log "Installing new cookie (#{tcn} : #{cookie.hid})"
             cookies.permanent[tcn] = cookie.hid
-            log "Cookie marked as new"
+            E9Crm.log "Cookie marked as new"
             cookie.new_visit = true
           end
         end
 
-        log "Final Cookie : #{@_tracking_cookie.inspect}"
+        E9Crm.log("Final Cookie : #{@_tracking_cookie.inspect}")
 
         @_tracking_cookie
       end
