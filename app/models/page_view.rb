18cb90f7bc874602cef2f7983573cc183e3ab938
Travis <numbers1311407@gmail.com>
2011-05-11 14:31:28 -0400

    added by_user and by_campaign scopes to page_view

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -14,9 +14,23 @@
 #
 class PageView < ActiveRecord::Base
   belongs_to :tracking_cookie
+
   has_one :user, :through => :tracking_cookie
   has_one :campaign, :inverse_of => :page_views
 
   scope :new_visits,    lambda { |v=true| where(:new_visit => v) }
   scope :repeat_visits, lambda { new_visits(false) }
+
+  scope :by_user, lambda {|*users| 
+    users.flatten!
+    users.map! &:to_param
+    joins(:tracking_cookie).where(TrackingCookie.arel_table[:user_id].send *(users.length == 1 ? [:eq, users.pop] : [:in, users]))
+  }
+
+  scope :by_campaign, lambda {|*campaigns| 
+    campaigns.flatten!
+    campaigns.map! &:to_param
+    where(arel_table[:campaign_id].send *(campaigns.length == 1 ? [:eq, campaigns.pop] : [:in, campaigns]))
+  }
+
 end
